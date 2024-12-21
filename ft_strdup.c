eee8556c8793e4cfb13985ff53de8c7f65b2170d
BlanckSpeed <inrodrigoant@gmail.com>
2024-12-21 07:39:45 +0100

    fix: corrected functions without gnl

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: rlendine <rlendine@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 07:28:31 by rodrigo           #+#    #+#             */
-/*   Updated: 2024/12/21 02:08:48 by rodrigo          ###   ########.fr       */
+/*   Updated: 2024/12/21 07:33:03 by rlendine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,19 @@ char	*ft_strdup(const char *s1)
 	dp[i] = '\0';
 	return (dp);
 }
+/*
+int main() {
+    char *original = "Hola, Mundo!";
+    char *copia = ft_strdup(original);
+
+    if (copia) {
+        printf("Original: %s\n", original);
+        printf("Copia: %s\n", copia);
+        free(copia);  // Liberar memoria
+    } else {
+        printf("Error al duplicar la cadena.\n");
+    }
+
+    return 0;
+}
+*/
